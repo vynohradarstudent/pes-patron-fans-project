1123f2479a346624e82328f791f00c347504d124
Nikita Gamaiunov <nikita_gamaiunov@epam.com>
2024-06-10 02:23:36 +0300

    Added header example

---
+++ example/header.js
@@ -0,0 +1,62 @@
+$(document).ready(async function () {
+
+    let userData = await getAuthorize();
+
+    if  (userData != null){
+        $(".auth").hide();
+        $(".user-info").show();
+
+        $("#username").text(userData.displayName);
+        console.log(userData)
+        if  (userData.role.name === "ADMIN"){
+            $("header nav ul").append(`<li> <a href="#1"> Admin function 1 </a>  </li>`);
+            $("header nav ul").append(`<li> <a href="#2"> Admin function 2 </a>  </li>`);
+        }
+
+    } else {
+        $(".auth").show();
+        $(".user-info").hide();
+    }
+
+    $("#logout-btn").on("click", async function () {
+        localStorage.setItem("accessToken", null);
+        localStorage.setItem("refreshToken", null);
+
+        window.location.reload();
+    });
+});
+
+
+async function getAuthorize() {
+    let token = {
+        accessToken: localStorage.getItem("accessToken"),
+        refreshToken: localStorage.getItem("refreshToken")
+    }
+
+    if (token.accessToken !== 'null') {
+        try {
+            let userData =  getMe(token.accessToken);
+
+            if (userData.status !== 401) {
+                return userData;
+            } else {
+                let newTokens = await refreshToken(token.refreshToken);
+
+                if (newTokens) {
+                    localStorage.setItem("accessToken", newTokens.accessToken);
+                    localStorage.setItem("refreshToken", newTokens.refreshToken);
+
+                    let newUserData = await getMe(newTokens.accessToken);
+                    return newUserData;
+                }
+            }
+        } catch (error) {
+            console.error("Error during authorization process", error);
+        }
+    }
+
+    localStorage.setItem("accessToken", null);
+    localStorage.setItem("refreshToken", null);
+
+    return null;
+}
--- example/login-page.js
+++ example/login-page.js
@@ -1,14 +1,17 @@
 $(document).ready(function () {
 
     $("#login").on("click", async function () {
-        let email = $("#email").val()
-        let password = $("#password").val()
+        let email = $("#email").val();
+        let password = $("#password").val();
 
         let token = await login({
             email: email,
             password: password
-        })
+        });
 
-        console.log(token)
+        localStorage.setItem("accessToken", token.accessToken);
+        localStorage.setItem("refreshToken", token.refreshToken);
+
+        window.location.reload();
     });
 });
